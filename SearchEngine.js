de86a829dde67ae5e41af24010c09ae8975c09b6
Alicamara20 <ali.camara1955@gmail.com>
2024-08-15 15:54:54 +0100

    My third commit

--- SearchEngine.js
+++ SearchEngine.js
@@ -10,43 +10,74 @@ const accessKey = "tl8BMn-MvrJdTZSIwnXqHKtfjpCMZF5-QFFeE3COciw";
 
 
 async function imageSearchEngine(){
+
+    
     imageName = searchInput.value;
     const response = await fetch(`https://api.unsplash.com/search/photos?page=${page}&query=${imageName}&client_id=${accessKey}&per_page=12`);
     const data = await response.json();
     console.log(data);
     const results = data.results;
-
-    if(data.results.length < 1){
-        searchResult.innerHTML = "Result Not Found";
-        searchResult.style.fontSize = "70px";
-        searchResult.style.margin = "auto";
-
-        searchResult.classList.add("error");
-        
+    
+    /*
+    if(resultLength < 1){
+        searchResult.innerHTML = "Result Not Found";        
+        searchResult.classList.add("error");   
     }
-
-    else{
+        */
+    
+    
         results.map( result => {
+    
+            const imageDiv = document.createElement("div");
+            imageDiv.className = "image-div";
+
             const link = document.createElement("a");
             link.target = "_blank";
             link.href = result.links.html;
+
             const resultImg = document.createElement("img");
             resultImg.src = result.urls.small;
-    
+        
             link.appendChild(resultImg);
-            searchResult.appendChild(link);
+            imageDiv.appendChild(link);
+            searchResult.appendChild(imageDiv);
+
+            let info = document.createElement("div");
+            info.className = "info";
+
+            let likes = document.createElement("div");
+            likes.className = "likes";
+            likes.innerHTML = `<i class="fa-sharp fa-regular fa-heart"></i> 
+                               <span> ${result.likes}</span>`;
+
+            let downloadBtn = document.createElement("div");
+            downloadBtn.className = "download-btn";
+            downloadBtn.innerHTML = `<i class="fa-sharp fa-regular fa-arrow-down-to-line"></i> <a href = ${result.links.download}>Download</a>`;
+
+            info.appendChild(likes);
+            info.appendChild(downloadBtn);
+            imageDiv.appendChild(info);
+
+
             
-        })
+        })   
     
-        showMore_btn.style.display = "block";
-    }
+                
+    
+        
+    showMore_btn.style.display = "block";
+}
+    
+
+
+
 
     
     
 
 
     
-}    
+    
 
     
 
