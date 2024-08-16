d4e00f5c06c35de591c635a133900c1d29d9e723
Alicamara20 <ali.camara1955@gmail.com>
2024-08-16 02:02:22 +0100

    my seventh commit

--- SearchEngine.js
+++ SearchEngine.js
@@ -47,13 +47,18 @@ async function imageSearchEngine(){
 
             let likes = document.createElement("div");
             likes.className = "likes";
-            likes.innerHTML = `<i class="fa-solid fa-love"></i> 
+            likes.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="m480-120-58-52q-101-91-167-157T150-447.5Q111-500 95.5-544T80-634q0-94 63-157t157-63q52 0 99 22t81 62q34-40 81-62t99-22q94 0 157 63t63 157q0 46-15.5 90T810-447.5Q771-395 705-329T538-172l-58 52Zm0-108q96-86 158-147.5t98-107q36-45.5 50-81t14-70.5q0-60-40-100t-100-40q-47 0-87 26.5T518-680h-76q-15-41-55-67.5T300-774q-60 0-100 40t-40 100q0 35 14 70.5t50 81q36 45.5 98 107T480-228Zm0-273Z"/></svg> 
                                <span> ${result.likes}</span>`;
 
             let downloadBtn = document.createElement("div");
             downloadBtn.className = "download-btn";
-            downloadBtn.innerHTML = `<a href = ${result.links.download} target = "_blank"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M480-320 280-520l56-58 104 104v-326h80v326l104-104 56 58-200 200ZM240-160q-33 0-56.5-23.5T160-240v-120h80v120h480v-120h80v120q0 33-23.5 56.5T720-160H240Z"/></svg> <span>Download</span></a>`;
+            downloadBtn.innerHTML = `<a href = ${result.links.download} target = "_blank"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M480-320 280-520l56-58 104 104v-326h80v326l104-104 56 58-200 200ZM240-160q-33 0-56.5-23.5T160-240v-120h80v120h480v-120h80v120q0 33-23.5 56.5T720-160H240Z"/></svg>
+                                     <span>Download</span></a>`;
+             
+            let imageDescription = document.createElement("p");
+            imageDescription.innerText = result.alt_description;                         
 
+            
             info.appendChild(likes);
             info.appendChild(downloadBtn);
             imageDiv.appendChild(info);
@@ -72,11 +77,11 @@ async function imageSearchEngine(){
 
             let userFullName = document.createElement("h3");
             let username = document.createElement("span");
-            let imageDescription = document.createElement("p");
             
-            userImage.src  = result.user.profile_image.small;
+            userImage.src  = result.user.profile_image.medium;
             userFullName.innerText =  result.user.name;
             username.innerText = result.user.username;
+
             
             userImageBox.appendChild(userImage);
             userInfo.appendChild(userFullName);
@@ -126,9 +131,7 @@ searchForm.addEventListener("submit", e => {
 
 })
 
- function displayImages(){
-
- }
+ 
 
  showMore_btn.addEventListener("click", () => {
     page++;
